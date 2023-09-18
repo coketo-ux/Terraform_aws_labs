d3c4ccab5234b1b6264a4c1d8dc8ab3fa35947d6
benejuzar21 <alvaro.gonzalezcosta@plexus.tech>
2023-09-18 16:23:44 +0200

    Prueba

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2_instance1" {
   ami                         = var.imagenes.redhat
   instance_type               = "t2.micro"
-  count                       = 0
+  count                       = 3
   key_name                    = aws_key_pair.key_class2.key_name
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id, aws_security_group.allow_http.id]
   subnet_id                   = element(aws_subnet.subnets_public.*.id, count.index)
@@ -15,7 +15,7 @@ resource "aws_instance" "ec2_instance1" {
 resource "aws_instance" "ec2_instance2" {
   ami                         = var.imagenes.windows
   instance_type               = "t2.micro"
-  count                       = 0
+  count                       = 1
   key_name                    = aws_key_pair.key_class2.key_name
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id, aws_security_group.allow_http.id]
   subnet_id                   = aws_subnet.subnets_public[1].id
